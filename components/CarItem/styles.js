3364bd52793e2c3f528616788f9d6a0c13b59a1c
Anıl <anil@Anl-MacBook-Air.local>
2023-09-14 14:57:53 +0300

    car item component

---
+++ components/CarItem/styles.js
@@ -0,0 +1,27 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  carContainer: {
+    width: "100%",
+    height: "100%",
+  },
+  titles: {
+    marginTop: "30%",
+    width: "100%",
+    alignItems: "center",
+  },
+  subtitle: {
+    fontSize: 16,
+    color: "#5C5E62",
+  },
+  title: {
+    fontSize: 40,
+    fontWeight: "500",
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "cover",
+    position: "absolute",
+  },
+});
